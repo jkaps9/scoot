b14d92f0c43fc82e0742d592c051b45a17ba9624
Josh <15219491+jkaps9@users.noreply.github.com>
2025-10-02 23:26:06 -0400

    add starter and config files

---
+++ .eleventy.js
@@ -0,0 +1,62 @@
+const path = require("node:path");
+const sass = require("sass");
+const MarkdownIt = require("markdown-it");
+
+module.exports = (config) => {
+  // add SCSS template format
+  config.addTemplateFormats("scss");
+
+  // Set directories to pass through to the dist folder
+  config.addPassthroughCopy("./src/assets");
+  config.addPassthroughCopy("./src/scripts");
+  config.addPassthroughCopy("./src/main.js");
+
+  // Configure SCSS files
+  config.addExtension("scss", {
+    outputFileExtension: "css",
+
+    // opt-out of Eleventy Layouts
+    useLayouts: false,
+
+    compile: async function (inputContent, inputPath) {
+      let parsed = path.parse(inputPath);
+      // Don’t compile file names that start with an underscore
+      if (parsed.name.startsWith("_")) {
+        return;
+      }
+
+      let result = sass.compileString(inputContent, {
+        loadPaths: [parsed.dir || ".", this.config.dir.includes],
+      });
+
+      // Map dependencies for incremental builds
+      this.addDependencies(inputPath, result.loadedUrls);
+
+      return async (data) => {
+        return result.css;
+      };
+    },
+  });
+
+  config.addFilter("commaize", function (num) {
+    return num.toLocaleString("en-us"); // Use "en-us" for comma separators
+  });
+
+  const md = new MarkdownIt({
+    html: true,
+  });
+
+  config.addFilter("markdown", (content) => {
+    return md.render(content);
+  });
+
+  return {
+    markdownTemplateEngine: "njk",
+    dataTemplateEngine: "njk",
+    htmlTemplateEngine: "njk",
+    dir: {
+      input: "src",
+      output: "dist",
+    },
+  };
+};
