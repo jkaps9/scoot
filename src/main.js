d21b33e1d34abcfbaf2649baf36ed4c324c7d9cf
Josh <15219491+jkaps9@users.noreply.github.com>
2025-10-06 01:21:17 -0400

    chevron toggle answer visibility

--- src/main.js
+++ src/main.js
@@ -1,1 +1,13 @@
-console.log("JAVASCRIPT!")
+console.log("JAVASCRIPT!");
+
+// FAQs
+
+const faqItems = document.querySelectorAll(".faq__item");
+
+faqItems.forEach((faqItem) => {
+  const chevron = faqItem.querySelector(".faq__item--title-row .chevron");
+  const answer = faqItem.querySelector(".faq__item--answer");
+  chevron.addEventListener("click", () => {
+    answer.classList.toggle("visible");
+  });
+});
